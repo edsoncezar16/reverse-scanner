a0a94f7695d3846f66a7689f532f897c95895a0a
Edson Nogueira <edsoncezar16@gmail.com>
2023-02-11 21:28:31 -0300

    Add BGR to RGB conversion before pytesseract.

--- scripts/scanner.py
+++ scripts/scanner.py
@@ -3,8 +3,9 @@ import pytesseract
 import pandas as pd
 
 image = cv2.imread("test_image_cropped.jpg")
-image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-text = pytesseract.image_to_string(image)
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # needed because by default
+# OpenCV stores images in BGR format and pytesseract assumes RGB format,
+text = pytesseract.image_to_string(image_rgb)
 rows = text.split("\n")
 table = [row.split("\t") for row in rows]
 image_data = pd.DataFrame(table[1:], columns=table[0])
